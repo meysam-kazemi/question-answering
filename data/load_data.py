a3380e794b89d1b7bc78dac41d0bde22196c0743
meysam-kazemi <meisam.daysu@gmail.com>
2025-01-20 17:08:24 +0330

    load the data completed

--- data/load_data.py
+++ data/load_data.py
@@ -1,5 +1,34 @@
-from datasets import load_dataset
+"for load the dataset"
 
-dataset = load_dataset('json', data_files='data/raw/simple-data.json')
+import os
+import sys
+from datasets import load_dataset
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from src.utils import read_config, red_print
+
+dataset = load_dataset('data/raw/')
 
 print(dataset)
+
+def load_the_dataset(config):
+    """
+    for load dataset locally
+    Let’s assume the data is in JSON format.
+    """
+    try:
+        path = config["dataset"]['path']
+    except:
+        red_print("[Config Error!] load_the_dataset")
+        return False
+    try:
+        return load_dataset(path)
+    except:
+        red_print('Loading the data Failed!!')
+        return False
+    
+
+# Example usage:
+if __name__ == "__main__":
+    config = read_config()
+    data = load_the_dataset(config)
+    print(data)
