32b01ed11a74eba36c7661e3c757ff91df46974b
meysam-kazemi <meisam.daysu@gmail.com>
2025-01-23 15:34:41 +0330

    use arguments

--- src/inference.py
+++ src/inference.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import argparse
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from src.model_and_tokenizer import modelAndTokenizer
 from src.utils import read_config, red_print, chatQwen
@@ -11,6 +12,17 @@ class inference:
     def __call__(self, prompt):
         res = self.chat(prompt)
         return res
+
+def parse_arguments():
+    """Parse command-line arguments."""
+    parser = argparse.ArgumentParser(description="Run inference with the Qwen model.")
+    parser.add_argument(
+        '--prompt', 
+        type=str, 
+        required=True, 
+        help='The prompt to send to the model.'
+    )
+    return parser.parse_args()
 
 if __name__=="__main__":
     config = read_config()
